19aed544c3d8499bc3616ca2f0c723dd8065c09e
Mattia-Rigotti <mattia.rigotti@live.it>
2014-06-06 18:39:13 +0200

    now supports a get request (/exec.php?id=x)
    which returns the output associated with the function with id x

--- exec.php
+++ exec.php
@@ -1,22 +1,35 @@
 <?php
 
 $addr = $_POST['url'];
+$req_id = $_GET['id'];
+$request = false;
+if ($req_id != "") {
+    $request = true;
+}
 $input = ['n1' => 2, 'n2' => 3];
 //print $addr."\n";
-$function = file_get_contents($addr);
+
 //print $function."\n";
 //$ex = exec("php -r '$function'");
+$directory = "/var/www/html/functions";
+if ($request) {
+    $FileName = $req_id. ".txt";
+    $FileHandle = fopen($directory . "/" . $FileName, 'r') or die("can't open file");
+    $addr=fgets($FileHandle);
+}
+$function = file_get_contents($addr);
 eval($function);
 //print $ex;
 $result = main($input);
 print $result;
-$directory = "/var/www/html/functions";
-$filelist = scandir($directory, $sorting_order = SCANDIR_SORT_ASCENDING);
-$max_id_arr = explode(".", end($filelist));
-$max_id = $max_id_arr[0];
-$FileName = ((int) $max_id + 1) . ".txt";
-$FileHandle = fopen($directory."/".$FileName, 'w') or die("can't open file");
-$txt = $addr . "\n" . $result;
-fwrite($FileHandle, $txt);
-fclose($FileHandle);
+if (!$request) {
+    $filelist = scandir($directory, $sorting_order = SCANDIR_SORT_ASCENDING);
+    $max_id_arr = explode(".", end($filelist));
+    $max_id = $max_id_arr[0];
+    $FileName = ((int) $max_id + 1) . ".txt";
+    $FileHandle = fopen($directory . "/" . $FileName, 'w') or die("can't open file");
+    $txt = $addr . "\n" . $result;
+    fwrite($FileHandle, $txt);
+    fclose($FileHandle);
+}
 ?>
