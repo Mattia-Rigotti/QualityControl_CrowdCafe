ed4f4ed903706015bfc45800e81b0450c32fdd03
Mattia-Rigotti <mattia.rigotti@live.it>
2014-06-06 15:29:08 +0200

    files are now saved in the server for every function

--- exec.php
+++ exec.php
@@ -1,12 +1,22 @@
 <?php
 
 $addr = $_POST['url'];
-$input = ['n1'=>2,'n2'=>3];
+$input = ['n1' => 2, 'n2' => 3];
 //print $addr."\n";
 $function = file_get_contents($addr);
 //print $function."\n";
 //$ex = exec("php -r '$function'");
 eval($function);
 //print $ex;
-print main($input);
+$result = main($input);
+print $result;
+$directory = "/var/www/html/functions";
+$filelist = scandir($directory, $sorting_order = SCANDIR_SORT_ASCENDING);
+$max_id_arr = explode(".", end($filelist));
+$max_id = $max_id_arr[0];
+$FileName = ((int) $max_id + 1) . ".txt";
+$FileHandle = fopen($directory."/".$FileName, 'w') or die("can't open file");
+$txt = $addr . "\n" . $result;
+fwrite($FileHandle, $txt);
+fclose($FileHandle);
 ?>
