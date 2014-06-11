f116c396a289453f3244894a309fc59f70dc998b
Mattia-Rigotti <mattia.rigotti@live.it>
2014-06-11 13:14:02 +0200

    added css and now uses a db instead of files

--- exec.php
+++ exec.php
@@ -1,5 +1,7 @@
 <?php
 
+include "db_connect.php";
+
 $addr = $_POST['url'];
 $req_id = $_GET['id'];
 $request = false;
@@ -16,28 +18,46 @@ if ($req_id != "") {
 } else {
     $input = ['n1' => 2, 'n2' => 3];
 }
-//print $addr."\n";
-//print $function."\n";
-//$ex = exec("php -r '$function'");
-$directory = "/var/www/html/functions";
+
+$database = new DB_Connector();
+if (!$database->connect()) {
+    echo "DB connection error!";
+}
+/* $directory = "/var/www/html/functions";
+  if ($request) {
+  $FileName = $req_id . ".txt";
+  $FileHandle = fopen($directory . "/" . $FileName, 'r') or die("can't open file");
+  $addr = fgets($FileHandle);
+  } */
+
 if ($request) {
-    $FileName = $req_id . ".txt";
-    $FileHandle = fopen($directory . "/" . $FileName, 'r') or die("can't open file");
-    $addr = fgets($FileHandle);
+    $url_fieldname = "url";
+    $q = "SELECT $url_fieldname FROM Functions WHERE id=$req_id";
+    $res = $database->query($q);
+    //$res = mysql_query($q);
+    $addr = mysql_fetch_assoc($res)[$url_fieldname];
 }
 $function = file_get_contents($addr);
 eval($function);
-//print $ex;
+
 $result = main($input);
 print $result;
 if (!$request) {
-    $filelist = scandir($directory, $sorting_order = SCANDIR_SORT_ASCENDING);
-    $max_id_arr = explode(".", end($filelist));
-    $max_id = $max_id_arr[0];
-    $FileName = ((int) $max_id + 1) . ".txt";
-    $FileHandle = fopen($directory . "/" . $FileName, 'w') or die("can't open file");
-    $txt = $addr . "\n" . $result;
-    fwrite($FileHandle, $txt);
-    fclose($FileHandle);
+    /* $filelist = scandir($directory, $sorting_order = SCANDIR_SORT_ASCENDING);
+      $max_id_arr = explode(".", end($filelist));
+      $max_id = $max_id_arr[0];
+      $FileName = ((int) $max_id + 1) . ".txt";
+      $FileHandle = fopen($directory . "/" . $FileName, 'w') or die("can't open file");
+      $txt = $addr . "\n" . $result;
+      fwrite($FileHandle, $txt);
+      fclose($FileHandle); */
+    $q = "INSERT INTO Functions(url) VALUES ('$addr')";
+    //mysql_query($q);
+    $database->query($q);
 }
+
+$database->disconnect();
+//print $addr."\n";
+//print $function."\n";
+//$ex = exec("php -r '$function'");
 ?>
