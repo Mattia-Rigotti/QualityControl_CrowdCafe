73288657123d57ca4a9d98dcdb98b6a9c92900d4
Mattia-Rigotti <mattia.rigotti@live.it>
2014-07-07 19:08:05 +0200

    Too mani changes to list

--- exec.php
+++ exec.php
@@ -3,20 +3,21 @@
 include "db_connect.php";
 
 $addr = $_POST['url'];
+$name = $_POST['name'];
+$code_tmp = $_POST['code'];
+$code = substr($code_tmp, 5, $code_tmp . length - 2);
 $req_id = $_GET['id'];
 $request = false;
 
 
 if ($req_id != "") {
     $request = true;
-    foreach ($_GET as $key => $value) {
-        if (strstr($key, "input_")) {
-            $input_names = explode("_", $key);
-            $input[$input_names[1]] = $value;
-        }
-    }
-} else {
-    $input = ['n1' => 2, 'n2' => 3];
+    /* foreach ($_GET as $key => $value) {
+      if (strstr($key, "input_")) {
+      $input_names = explode("_", $key);
+      $input[$input_names[1]] = $value;
+      }
+      } */
 }
 
 $database = new DB_Connector();
@@ -31,18 +32,36 @@ if (!$database->connect()) {
   } */
 
 if ($request) {
-    $url_fieldname = "url";
-    $q = "SELECT $url_fieldname FROM Functions WHERE id=$req_id";
+    $q = "SELECT url FROM Functions WHERE id=$req_id";
     $res = $database->query($q);
     //$res = mysql_query($q);
-    $addr = mysql_fetch_assoc($res)[$url_fieldname];
-}
-$function = file_get_contents($addr);
-eval($function);
+    $addr = mysql_fetch_assoc($res)["url"];
 
-$result = main($input);
-print $result;
-if (!$request) {
+    $foo = file_get_contents("php://input");
+    $input = json_decode($foo, true);
+    if ($addr != NULL) {
+        $function = file_get_contents($addr);
+    } else {
+        $q = "SELECT code FROM Functions WHERE id=$req_id";
+        $res = $database->query($q);
+        $function = mysql_fetch_assoc($res)["code"];
+    }
+
+    function main($input, $function) {
+        eval($function);
+    }
+
+    $result = main($input, $function);
+    print $result;
+} else {
+
+    //echo "prova";
+    $q = "SELECT MAX(id) FROM Functions";
+    $res = $database->query($q);
+    //$res = mysql_query($q);
+    $max_id = mysql_fetch_assoc($res)["MAX(id)"];
+    print "Function id: " . $max_id;
+
     /* $filelist = scandir($directory, $sorting_order = SCANDIR_SORT_ASCENDING);
       $max_id_arr = explode(".", end($filelist));
       $max_id = $max_id_arr[0];
@@ -51,9 +70,17 @@ if (!$request) {
       $txt = $addr . "\n" . $result;
       fwrite($FileHandle, $txt);
       fclose($FileHandle); */
-    $q = "INSERT INTO Functions(url) VALUES ('$addr')";
-    //mysql_query($q);
-    $database->query($q);
+    if ($addr != "") {
+        $q = "INSERT INTO Functions(url,name) VALUES ('$addr','$name')";
+        //mysql_query($q);
+        $database->query($q);
+    } else {
+        $code = addslashes($code);
+        $q = "INSERT INTO Functions(name,code) VALUES ('$name','$code')";
+        //mysql_query($q);
+        $database->query($q);
+    }
+    header("Location: index.php");
 }
 
 $database->disconnect();
