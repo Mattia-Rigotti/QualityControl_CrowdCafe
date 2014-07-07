c96869d6ab20170a286d4982b20d8d3f739f4673
Mattia-Rigotti <mattia.rigotti@live.it>
2014-07-07 19:09:39 +0200

    Revert "Too mani changes to list"
    This reverts commit 73288657123d57ca4a9d98dcdb98b6a9c92900d4.

--- exec.php
+++ exec.php
@@ -3,21 +3,20 @@
 include "db_connect.php";
 
 $addr = $_POST['url'];
-$name = $_POST['name'];
-$code_tmp = $_POST['code'];
-$code = substr($code_tmp, 5, $code_tmp . length - 2);
 $req_id = $_GET['id'];
 $request = false;
 
 
 if ($req_id != "") {
     $request = true;
-    /* foreach ($_GET as $key => $value) {
-      if (strstr($key, "input_")) {
-      $input_names = explode("_", $key);
-      $input[$input_names[1]] = $value;
-      }
-      } */
+    foreach ($_GET as $key => $value) {
+        if (strstr($key, "input_")) {
+            $input_names = explode("_", $key);
+            $input[$input_names[1]] = $value;
+        }
+    }
+} else {
+    $input = ['n1' => 2, 'n2' => 3];
 }
 
 $database = new DB_Connector();
@@ -32,36 +31,18 @@ if (!$database->connect()) {
   } */
 
 if ($request) {
-    $q = "SELECT url FROM Functions WHERE id=$req_id";
+    $url_fieldname = "url";
+    $q = "SELECT $url_fieldname FROM Functions WHERE id=$req_id";
     $res = $database->query($q);
     //$res = mysql_query($q);
-    $addr = mysql_fetch_assoc($res)["url"];
+    $addr = mysql_fetch_assoc($res)[$url_fieldname];
+}
+$function = file_get_contents($addr);
+eval($function);
 
-    $foo = file_get_contents("php://input");
-    $input = json_decode($foo, true);
-    if ($addr != NULL) {
-        $function = file_get_contents($addr);
-    } else {
-        $q = "SELECT code FROM Functions WHERE id=$req_id";
-        $res = $database->query($q);
-        $function = mysql_fetch_assoc($res)["code"];
-    }
-
-    function main($input, $function) {
-        eval($function);
-    }
-
-    $result = main($input, $function);
-    print $result;
-} else {
-
-    //echo "prova";
-    $q = "SELECT MAX(id) FROM Functions";
-    $res = $database->query($q);
-    //$res = mysql_query($q);
-    $max_id = mysql_fetch_assoc($res)["MAX(id)"];
-    print "Function id: " . $max_id;
-
+$result = main($input);
+print $result;
+if (!$request) {
     /* $filelist = scandir($directory, $sorting_order = SCANDIR_SORT_ASCENDING);
       $max_id_arr = explode(".", end($filelist));
       $max_id = $max_id_arr[0];
@@ -70,17 +51,9 @@ if ($request) {
       $txt = $addr . "\n" . $result;
       fwrite($FileHandle, $txt);
       fclose($FileHandle); */
-    if ($addr != "") {
-        $q = "INSERT INTO Functions(url,name) VALUES ('$addr','$name')";
-        //mysql_query($q);
-        $database->query($q);
-    } else {
-        $code = addslashes($code);
-        $q = "INSERT INTO Functions(name,code) VALUES ('$name','$code')";
-        //mysql_query($q);
-        $database->query($q);
-    }
-    header("Location: index.php");
+    $q = "INSERT INTO Functions(url) VALUES ('$addr')";
+    //mysql_query($q);
+    $database->query($q);
 }
 
 $database->disconnect();
--- js/scripts.js
+++
@@ -1,7 +0,0 @@
-$('[data-toggle=offcanvas]').click(function() {
-  $('.row-offcanvas').toggleClass('active');
-});
-
-$('.btn-toggle').click(function() {
-  $(this).find('.btn').toggleClass('active').toggleClass('btn-default').toggleClass('btn-primary');
-});
